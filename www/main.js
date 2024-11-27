b811c2e3361dea8d58469678499a0440caa943f1
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-27 11:12:43 +0530

    some changes

--- www/main.js
+++ www/main.js
@@ -14,29 +14,34 @@ $(document).ready(function () {
     // Siri Configuration
     var siriWave = new SiriWave({
         container: document.getElementById("siri-container"),
-        width: 800,
+        width: window.innerWidth * 0.8,
         height: 200,
         style: "ios9",
         frequency: 4,
-        amplitude: "1",
-        speed: "0.250",
-        autostart: true
-      });
+        amplitude: 2,
+        speed: 0.2,
+        autostart: true,
+        color: '#BB86FC',
+    });
+
 
       //siri message animation
       $('.siri-message').textillate({
         loop: true,
-        sync: true,
         in: {
             effect: "fadeInUp",
-            sync: true
+            delayScale: 1.5,
+            delay: 30,
+            sync: true,
         },
         out: {
             effect: "fadeOutUp",
-            sync:true
+            delayScale: 1.5,
+            delay: 30,
+            sync: true,
         },
-
     });
+
 
     // Mic Button Script
     $("#MicBtn").click(function (e) { 
