08e3ce2cca77761456422c3e8abe7f2f02af1f18
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-12-11 21:37:02 +0530

    Defining Tables in database

--- engine/db.py
+++ engine/db.py
@@ -1,3 +1,11 @@
 import sqlite3
 
 conn = sqlite3.connect("iris.db")
+cursor = conn.cursor()
+
+query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(100))"
+cursor.execute(query)
+
+query = "INSERT INTO sys_command VALUES (null,'','')"
+cursor.execute(query)
+conn.commit()
