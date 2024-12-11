4db3a4fde95b5efaf257649fe8060c404fb95117
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-12-11 21:50:05 +0530

    Writing a new query

--- engine/db.py
+++ engine/db.py
@@ -6,6 +6,13 @@ cursor = conn.cursor()
 query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(100))"
 cursor.execute(query)
 
-query = "INSERT INTO sys_command VALUES (null,'one note','C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.EXE')"
+#query = "INSERT INTO sys_command VALUES (null,'one note','C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.EXE')"
+#cursor.execute(query)
+#conn.commit()
+
+query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(100))"
+cursor.execute(query)
+
+query = "INSERT INTO web_command VALUES (null,'github','https://github.com/')"
 cursor.execute(query)
 conn.commit()
