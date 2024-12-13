aebdbaa662090b102d5c45178969dfc4e0300506
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-12-13 21:59:21 +0530

    Solving app opening issues

--- engine/db.py
+++ engine/db.py
@@ -6,13 +6,13 @@ cursor = conn.cursor()
 query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(100))"
 cursor.execute(query)
 
-#query = "INSERT INTO sys_command VALUES (null,'one note','C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.EXE')"
-#cursor.execute(query)
-#conn.commit()
+query = "INSERT INTO sys_command VALUES (null,'whatsapp','C:\\Program Files\\WindowsApps\\5319275A.WhatsAppDesktop_2.2445.7.0_x64__cv1g1gvanyjgm\\WhatsApp.exe')"
+cursor.execute(query)
+conn.commit()
 
 query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(100))"
 cursor.execute(query)
 
-query = "INSERT INTO web_command VALUES (null,'chat gpt','https://chatgpt.com/')"
-cursor.execute(query)
-conn.commit()
+#query = "INSERT INTO web_command VALUES (null,'chat gpt','https://chatgpt.com/')"
+#cursor.execute(query)
+#conn.commit()
